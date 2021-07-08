a94abe6d7c9a96c4017a4f514b1ea4c08a9487e9
Minwoong <kevinkim0617kim@gmail.com>
2021-07-08 15:42:46 +0900

    dbConnection Test

---
+++ dbConnectionTest/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://dbmasteruser:password@ls-d8e66d0492f2c70ce3ecd3e603cf642a8c8a8351.cqobb3tz8sun.ap-northeast-2.rds.amazonaws.com/Test'
+
+db=SQLAlchemy(app)
+
+class Student(db.Model):
+  __tablename__='students'
+  id=db.Column(db.Integer,primary_key=True)
+  fname=db.Column(db.String(40))
+  lname=db.Column(db.String(40))
+  pet=db.Column(db.String(40))
+
+  def __init__(self,fname,lname,pet):
+    self.fname=fname
+    self.lname=lname
+    self.pet=pet
+
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+  fname= request.form['fname']
+  lname=request.form['lname']
+  pet=request.form['pets']
+
+  student=Student(fname,lname,pet)
+  db.session.add(student)
+  db.session.commit()
+
+  #fetch a certain student2
+  studentResult=db.session.query(Student).filter(Student.id==1)
+  for result in studentResult:
+    print(result.fname)
+
+  return render_template('success.html', data=fname)
+
+
+if __name__ == '__main__':  #python interpreter assigns "__main__" to the file you run
+  app.run(debug=True)
+
+
+
+
+
