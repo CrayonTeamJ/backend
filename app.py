7f704e671deee169e10b4ddbdd0d7d7e454a90be
Minwoong <kevinkim0617kim@gmail.com>
2021-07-09 20:51:02 +0900

    Separate models, config, add lig in function

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from flask import Flask, render_template
-from flask_migrate import Migrate
+from flask import Flask, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from sqlalchemy.sql.elements import Null
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms import BooleanField, PasswordField, StringField
+from wtforms.validators import Email, InputRequired, Length
+import config
 from forms import LoginForm, RegisterForm
 
-migrate = Migrate()
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SECRET_KEY'] ='Thisissupportedtobesecret!'
+
+db = SQLAlchemy()
+
+migrate = Migrate() 
+
+  
+app.config.from_object(config)
+db.init_app(app)
+migrate.init_app(app, db)
+
+from models import user_info
 Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql+psycopg2://dbmasteruser:password@ls-d8e66d0492f2c70ce3ecd3e603cf642a8c8a8351.cqobb3tz8sun.ap-northeast-2.rds.amazonaws.com/USERinfo'
 
-
-class user_info(db.Model):
-    __tablename__='user_info'
-    user_sn = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.String(150), unique=True, nullable=False)
-    user_pw = db.Column(db.String(200), nullable=False)
-    user_nick = db.Column(db.String(120), unique=True, nullable=False)
-    user_prof = db.Column(db.Text, nullable=True)
-
-    def __init__(self, user_id, user_pw, user_nick, user_prof):
-        self.user_id=user_id
-        self.user_pw=user_pw
-        self.user_nick=user_nick
-        self.user_prof=user_prof
 
 
 @app.route('/')
@@ -41,7 +35,12 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+        user = user_info.query.filter_by(user_id=form.userID.data).first()
+        if user:
+            if user.user_pw == form.password.data:
+                return redirect(url_for('dashboard'))
+            
+        return '<h1>Invalid username of password</h1>'
 
     return render_template('login.html', form=form)
     
@@ -66,4 +65,4 @@ def dashboard():
     return render_template('dashboard.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager .run(debug=True)
--- forms.py
+++ forms.py
@@ -15,6 +15,6 @@ class RegisterForm(FlaskForm):
     
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    userID = StringField('userID', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
