d5b5d448052384db70ff0bd51fb2eafd8ec92054
POL6463 <kevinkim0617kim@gmail.com>
2021-07-13 01:25:53 +0900

    Add Hash function in Login, Signin

--- app.py
+++ app.py
@@ -1,38 +1,25 @@
+import sys, os
 from flask import Flask, redirect, render_template, url_for, jsonify, Response, make_response, request
-from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql.elements import Null
-from sqlalchemy.sql.expression import insert, true
-from werkzeug.wrappers import response
 import logging
+from werkzeug.utils import secure_filename
 import config
-
 
 app = Flask(__name__)
 db = SQLAlchemy()
 migrate = Migrate() 
 CORS(app) # 있어야 프런트와 통신 가능, 없으면 오류뜸
 
-  
 app.config.from_object(config)
 db.init_app(app)
 migrate.init_app(app, db)
 
-from models import user_info
 import views
 
 logging.basicConfig(level=logging.DEBUG )
 
-
-
-# @app.route('/api')
-# def index():
-#     print("hello world")
-#     app.logger.info('hello world - app logger ') 
-#     return Response("test", status=201, mimetype='text/html')
-#     #return make_response(jsonify({'success' : 'success'}), 200)
 
 
 
@@ -42,10 +29,10 @@ def login():
     UserLogin = views.user_login(userform['userID'], userform['password'])
     
     if UserLogin == True:
-        return make_response(jsonify({'Result' : 'Login Success'}), 200)
+        return make_response(jsonify({'Result' : 'Login_Success'}), 200)
             
     else:
-        return make_response(jsonify({'Result' : 'Login Fail'}), 203)
+        return make_response(jsonify({'Result' : 'Login_Fail'}), 203)
 
     
 @app.route('/api/signup', methods=['POST'])
--- views.py
+++ views.py
@@ -6,16 +6,22 @@ from sqlalchemy.sql.expression import false
 os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 import models
 from app import app, db
+import bcrypt
+
 
 userDB = models.user_info
+
+
 
 
 def user_insert(userID, userPW, userNICK):
     user_byID = userDB.query.filter(userID == userDB.user_id).first()
     user_byNK = userDB.query.filter(userNICK == userDB.user_nick).first()
+    bytes_password = userPW.encode('UTF-8')
+    bytes_hashed_password = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
     
     if user_byID is None and user_byNK is None:
-        new_user = userDB(userID, userPW, userNICK)
+        new_user = userDB(userID, bytes_hashed_password.decode('UTF-8'), userNICK)
         db.session.add(new_user)
         db.session.commit()
         result = 'success'
@@ -33,10 +39,12 @@ def user_insert(userID, userPW, userNICK):
         
 
 def user_login(userID, password):
+    bytes_password = password.encode('utf-8')
+
 
     user_byID = userDB.query.filter(userID == userDB.user_id).first()
     if user_byID:
-        if user_byID.user_pw == password:
+        if bcrypt.checkpw(bytes_password, user_byID.user_pw.encode('utf-8')):
             return True
     else :
         False
