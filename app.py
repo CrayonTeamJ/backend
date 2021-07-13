177c0ec5fa027a57fbeecfa5c7d77f739f4e36f9
POL6463 <kevinkim0617kim@gmail.com>
2021-07-13 16:36:50 +0900

    Set refresh cookie, access cookies

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import *
 import logging
 from werkzeug.utils import secure_filename
 import config
@@ -15,6 +18,10 @@ migrate = Migrate()
 CORS(app) # 있어야 프런트와 통신 가능, 없으면 오류뜸
 jwt = JWTManager(app)
 
+JWT_COOKIE_SECURE = False # https를 통해서만 cookie가 갈 수 있는지 (production 에선 True)
+JWT_TOKEN_LOCATION = ['cookies']
+JWT_COOKIE_CSRF_PROTECT = True 
+
 app.config.from_object(config)
 db.init_app(app)
 migrate.init_app(app, db)
@@ -22,7 +29,32 @@ migrate.init_app(app, db)
 import views
 
 logging.basicConfig(level=logging.DEBUG )
-#
+
+
+@app.route('/api/input', methods=['GET'])
+@jwt_required()
+def user_only():
+	cur_user = get_jwt_identity()
+	if cur_user is None:
+		return make_response(jsonify({'Result' : 'Fail', 'message' : 'Not_user'}), 203)
+	else:
+		return make_response(jsonify({'Result' : 'Success', 'message' : 'Is_user'}), 200)
+
+@app.route('/api/input', methods=['POST'])
+def video_input():
+
+    if request.form['image_type'] == "1" :
+        Your_input = request.files['file']
+        video_filename=secure_filename(Your_input.filename)
+        Your_input.save(os.path.join('./data/',video_filename))
+        return make_response(jsonify({'Result' : 'Success'}), 200)
+
+        # gcp_control.upload_blob_filename('teamg-data','./data/'+video_filename,video_filename)
+        # video_path = 'https://storage.googleapis.com/teamg-data/'+video_filename
+        # video_path_signed = gcp_control.generate_download_signed_url_v4('teamg-data', video_filename)
+        # os.remove('./data/'+video_filename)
+        # views.video_insert('local',video_filename,video_path_signed)
+
 
 @app.route('/api/login', methods=['POST'])
 def login():
@@ -30,7 +62,15 @@ def login():
     UserLogin = views.user_login(userform['userID'], userform['password'])
     
     if UserLogin == True:
-        return make_response(jsonify(Result = "success", access_token = create_access_token(identity = userform['userID'], expires_delta = False)))
+        access_token = create_access_token(identity=userform['userID'])
+        refresh_token = create_refresh_token(identity=userform['userID'])
+
+        resp = jsonify({'login': True})
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
+        return resp, 200
+        
+        #return make_response(jsonify(Result = "success", access_token = create_access_token(identity = userform['userID'], expires_delta = False)))
         #return make_response(jsonify({'Result' : 'Login_Success',}, access_token = create_access_token(identity = userform['userID'], expires_delta = False)))
             
     else:
