305fe98a5c024b88cdbcd83d046f5628ad36ad4d
POL6463 <kevinkim0617kim@gmail.com>
2021-07-19 16:54:58 +0900

    aa

--- README.md
+++
@@ -1,7 +0,0 @@
-backend
-
-확인중입니다.
-확인이요
-
-minwoongnimeun mikookin
-이유정
--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-JWT_SECRET_KEY = "I'M IML"
-SECRET_KEY ='Thisissupportedtobesecret!'
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dbmasteruser:password@ls-d8e66d0492f2c70ce3ecd3e603cf642a8c8a8351.cqobb3tz8sun.ap-northeast-2.rds.amazonaws.com/USERinfo'.format(os.path.join(BASE_DIR, 'app.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
