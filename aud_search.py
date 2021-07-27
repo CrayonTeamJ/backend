7926218cec1355f5a0da3e1e1f1bfcb9eee84f23
Maeng <msuyun1010@ewhain.net>
2021-07-27 20:54:56 +0900

    add query for aud_search

--- aud_search.py
+++ aud_search.py
@@ -4,9 +4,7 @@ from flask import json, jsonify
 es = Elasticsearch(['http://elasticsearch:9200'], http_auth=('elastic', 'changeme'))
 
 def createIndex():
-        # ===============
-        # 인덱스 생성
-        # ===============
+
         es.indices.create(
             index = "content",
             body = {
@@ -49,29 +47,58 @@ def audio_search(video_id, keyword):
     # index = [검색할_인덱스]
     # query_body = [검색할_쿼리문]
 
-
- # video_pk가 18이고 sentence에 “고양이”가 나오는 start time을 말해줘!
     query= {
-        "query": {
-            "bool": {
-                "must": [
-                    {
-                        "match": {
-                            "video_number": video_id
-                        }
-                    },
-                        {
-                        "match": {
-                            "sentence_list.sentence": keyword
+                "query": {
+                    "nested":{
+                        "path": "sentence_list",
+                        "query": {
+                            "bool": {
+                                "must": [
+                                    {
+                                        "match": {
+                                            "video_number": video_id
+                                        }
+                                    },
+                                    {
+                                        "match": {
+                                            "sentence_list.sentence": search_aud
+                                        }
+                                    }
+                                ]
+                            }
                         }
                     }
-                ]
-            }
-        },
-        "_source": ["start_time"]
+                },
+                "_source": ["start_time"]
     }
 
     res = es.search(index='', body=query)
     # res에 검색 결과가 담겨져 있다           
 
     return res  
+
+
+    # {
+#     "query": {
+#                 "bool": {
+#                     "must": [
+#                         {
+#                             "match": {
+#                                 "video_number": video_id
+#                             }
+#                         },
+#                         "query": {
+#                             "nested": {
+#                                 "path": "sentence_list" 
+#                                     {
+#                                         "match": {
+#                                             "sentence_list.sentence": search_aud
+#                                          }
+#                                      }
+#                             }
+#                         }
+#                     ]
+#                 }
+#     },
+#     "_source": ["start_time"]
+# }
